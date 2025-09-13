316e3d7b93ad57812b8204ebd634c74cdfd16957
Trisztán Piller <trisztan.piller@snapsoft.io>
2025-09-13 22:33:35 +0200

    fix: pnpm

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -8,6 +8,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
+import * as Joi from "joi";
+
 @Module({
   imports: [
     PrismaModule.forRoot({ isGlobal: true }),
