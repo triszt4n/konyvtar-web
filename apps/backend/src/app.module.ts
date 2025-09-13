1c6e3ce687eb1f5e10dca5a39f58ea8b7a9a0012
Trisztán Piller <trisztan.piller@snapsoft.io>
2025-09-13 21:28:56 +0200

    fix: dockerfile

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,11 +1,32 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true })],
+  imports: [
+    PrismaModule.forRoot({ isGlobal: true }),
+    CacheModule.register(),
+    ConfigModule.forRoot(),
+    EventEmitterModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        FRONTEND_CALLBACK: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        POSTGRES_PRISMA_URL: Joi.string().required(),
+        AUTHSCH_CLIENT_ID: Joi.string().required(),
+        AUTHSCH_CLIENT_SECRET: Joi.string().required(),
+      }),
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
